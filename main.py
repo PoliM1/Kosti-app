ddf993e1cecbcfedea96325565c69bb0e2d1eeb1
PoliM1 <kazancevap60@gmail.com>
2026-01-16 23:40:54 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# main.py - Полный код FastAPI сервера для игры в кости
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import random
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/roll")
+async def roll_dice():
+    return {"dice": random.randint(1, 6)}
+
+@app.get("/game")
+async def get_game_state(player1: int = 0, player2: int = 0):
+    return {"player1": player1, "player2": player2}
